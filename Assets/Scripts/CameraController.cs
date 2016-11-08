4e04a4a6c8b77b7ba8d97aae768b7a25f5a8f804
Thao Vo <tvo5@mail.pima.edu>
2016-11-07 17:48:02 -0700

    Unity - Rolling a ball tutorial

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		//get camera position - the position of the ball
+		offset = transform.position - player.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	// LateUpdate run after items have been updated
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed; 
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () {
+	//where physics code will go
+		float moveHorizontal = Input.GetAxis("Horizontal"); 
+		float moveVertical = Input.GetAxis("Vertical");
+
+		//create movement with x, y, z
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		//see vector 3 above
+		rb.AddForce (movement * speed);
+	
+		}
+
+
+}
